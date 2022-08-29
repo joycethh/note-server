67539e1f785b3747a5fae1eb0e58d139cade0456
joycethh <joycethh@outlook.com>
2022-08-29 11:59:09 -0500

    fix DELETE method route and clean the file

--- routes/notes.js
+++ routes/notes.js
@@ -1,36 +1,22 @@
 import express from "express";
-import mongoose from "mongoose";
-
-import Note from "../models/noteModel.js";
+import {
+  getNotes,
+  createNote,
+  deleteNote,
+  getSingleNote,
+  updateNote,
+} from "../controllers/noteController.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json("get all notes route");
-});
+router.get("/", getNotes);
 
-router.post("/", async (req, res) => {
-  const { title, content } = req.body;
-  const newNote = new Note({ title, content });
+router.post("/", createNote);
 
-  try {
-    await newNote.save();
-    res.status(201).json(newNote);
-  } catch (error) {
-    res.status(409).json({ message: error.message });
-  }
-});
-
-router.delete("/", (req, res) => {
-  res.json("DELETE ");
-});
+router.delete("/:id", deleteNote);
 
 //TODO in the future
-router.get("/:id", (req, res) => {
-  res.json("get singe one ");
-});
+router.get("/:id", getSingleNote);
 
-router.patch("/:id", (req, res) => {
-  res.json("update one ");
-});
+router.patch("/:id", updateNote);
 export default router;
